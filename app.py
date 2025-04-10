40da0941539152e8b2b041753cc212f0ae7444f2
Daniel <d.montalvomorell2002@gmail.com>
2025-04-10 02:47:49 +0200

    fix API KEY

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ llm = LLM(
     model=f"groq/{modelo_groq}",
     temperature=0.5,
     base_url="https://api.groq.com/openai/v1",
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
 # Crear agentes
